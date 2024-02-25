1961ba98bab7d1f26a23068789e05ac5e8f381fc
Sahil873 <sahilp0873@gmail.com>
2024-02-25 22:06:42 +0530

    solved the nav toggle issue

--- navigation.js
+++ navigation.js
@@ -2,12 +2,17 @@ const nav = document.querySelector(".primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
-  let visiblity = nav.getAttribute("data-visible");
-  if (visiblity === "false") {
-    nav.setAttribute("data-visible", true);
-    navToggle.setAttribute("aria-expanded", true);
-  } else {
-    nav.setAttribute("data-visible", false);
-    navToggle.setAttribute("aria-expanded", false);
-  }
+  let visibility = nav.getAttribute("data-visible");
+
+  // Convert the attribute value to a boolean
+  let isVisible = visibility === "true";
+
+  // Toggle the visibility
+  isVisible = !isVisible;
+
+  // Set the updated visibility attribute
+  nav.setAttribute("data-visible", isVisible);
+
+  // Set the aria-expanded attribute based on the updated visibility
+  navToggle.setAttribute("aria-expanded", isVisible);
 });
